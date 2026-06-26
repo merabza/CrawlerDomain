75ed2cc93c09232aea6de3170e45efb30df3193c
merabza <merabza@gmail.com>
2026-06-26 07:41:08 +0400

    202606260739

--- CrawlerDbModels/TaskModel.cs
+++ CrawlerDbModels/TaskModel.cs
@@ -8,7 +8,5 @@ public sealed class TaskModel : ItemData
 {
     public int TaskId { get; set; }
     public required string TaskName { get; set; }
-    public string? ApiName { get; set; }
-
     public ICollection<TaskStartPoint> StartPoints { get; set; } = new HashSet<TaskStartPoint>();
 }
